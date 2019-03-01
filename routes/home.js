dfca33cca53ba6b550fcb8b4f26d056b6906d391
SlickJohnson <liwa.johnson@gmail.com>
2019-02-28 16:06:48 -0800

    Update / route

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// create a route for default endpoint
+router.get("/", (req, res) => {
+  res.send({
+    status: `Hi :>`
+  });
+});
+
 router.get("/status", (req, res) => {
   res.send({
     status: `Connected to the backend at ${process.env.API_URL}`
